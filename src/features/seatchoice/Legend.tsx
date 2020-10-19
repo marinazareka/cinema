8880d403c227a336b7ddb0fae64c6e8a590f03a8
Marina <marinazareka@gmail.com>
2020-10-19 13:14:17 +0300

    Small fixes

--- src/features/seatchoice/Legend.tsx
+++ src/features/seatchoice/Legend.tsx
@@ -8,21 +8,21 @@ import { SeatType, SeatTypeTitle } from '../../types/types';
 const LegendComponent: FunctionComponent = () => (
   <>
     <Legend>
-      <Seat disabled sample seatType={SeatType.Single} />
+      <Seat disabled sample seatType={SeatType.Single} title={SeatTypeTitle.single} />
       <span>{SeatTypeTitle.single}</span>
-      <Seat disabled sample seatType={SeatType.Double} />
+      <Seat disabled sample seatType={SeatType.Double} title={SeatTypeTitle.double} />
       <span>{SeatTypeTitle.double}</span>
-      <Seat disabled sample seatType={SeatType.Triple} />
+      <Seat disabled sample seatType={SeatType.Triple} title={SeatTypeTitle.triple} />
       <span>{SeatTypeTitle.triple}</span>
     </Legend>
     <Legend>
-      <Seat disabled seatType={SeatType.Single}>
+      <Seat disabled seatType={SeatType.Single} title="disabled due to covid-19 prevention">
         <FontAwesomeIcon icon={faVirusSlash} />
       </Seat>
       <span>disabled due to covid-19 prevention</span>
-      <Seat type="button" disabled seatType={SeatType.Single} />
+      <Seat type="button" disabled seatType={SeatType.Single} title="occupied" />
       <span>occupied</span>
-      <Seat type="button" sample disabled chosen seatType={SeatType.Single} />
+      <Seat type="button" sample disabled chosen seatType={SeatType.Single} title="your choice" />
       <span>your choice</span>
     </Legend>
   </>
